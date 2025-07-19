e7de0892eba792c9a266446dd1825b157fa955c2
Rodney Mupanduki <rodneymupanduki@gmail.com>
2025-07-20 00:12:31 +0200

    tailwind setup

--- apps/listings-app/src/app/app.ts
+++ apps/listings-app/src/app/app.ts
@@ -3,7 +3,7 @@ import { RouterModule } from '@angular/router';
 import { NxWelcome } from './nx-welcome';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [ RouterModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
